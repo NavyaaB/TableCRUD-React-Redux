d09ae11f494c220b9693bb2e6724b4aa0234fd0e
Navya Bachati <Navya Bachati>
2017-02-19 20:11:28 -0600

    Done adding a new row

---
+++ TableCURD/src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Table,Button} from 'react-bootstrap';
+import './App.css';
+class App extends Component {
+  constructor(props){
+    super(props)
+  //  this.state={value: ''}
+    this.state={table:""}
+
+    this.save=this.save.bind(this)
+
+  }
+  save(){
+    console.log(this.refs.tbl.rows.length)
+    let i=this.refs.tbl.rows.length
+    console.log(this.refs.num.value)
+    let num=this.refs.num.value
+    let row = this.refs.tbl.insertRow(i-2); // i is now dynamic
+    let cell0 = row.insertCell(0);
+    cell0.innerHTML = num;
+    this.refs.num.value=''
+
+    console.log(this.refs.fname.value)
+    let fname=this.refs.fname.value
+    let cell1 = row.insertCell(1);
+    cell1.innerHTML = fname;
+    this.refs.fname.value=''
+
+    console.log(this.refs.lname.value)
+    let lname=this.refs.lname.value
+    let cell2 = row.insertCell(1);
+    cell2.innerHTML = lname;
+    this.refs.lname.value=''
+
+    console.log(this.refs.mail.value)
+    let mail=this.refs.mail.value
+    let cell3 = row.insertCell(3);
+    cell3.innerHTML = mail;
+    this.refs.mail.value=''
+
+
+
+
+
+    console.log(this.refs.fname.value)
+
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Table CRUD Operations</h3>
+          <Table striped bordered condensed hover>
+     <thead>
+       <tr>
+         <th>#</th>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>E-mail</th>
+       </tr>
+     </thead>
+     <tbody ref="tbl">
+       <tr>
+         <td>1</td>
+         <td>Mark</td>
+         <td>Otto</td>
+         <td>mark@mdo</td>
+       </tr>
+       <tr>
+         <td>2</td>
+         <td>Jacob</td>
+         <td>Thornton</td>
+         <td>Jacob@fat</td>
+       </tr>
+       <tr>
+         <td><input type="text" ref="num" placeholder="Enter #" value={this.state.value}></input> </td>
+        <td> <input type="text" ref="fname" placeholder="Enter First Name"></input></td>
+          <td><input type="text" ref="lname" placeholder="Enter Last Name"></input> </td>
+         <td> <input type="email" ref="mail" placeholder="Enter email"></input></td>
+       </tr>
+       <tr>
+         <td colSpan="3"></td>
+         <td><Button bsStyle="primary" onClick={this.save}>Save</Button></td>
+       </tr>
+     </tbody>
+   </Table>
+      </div>
+    );
+  }
+}
+
+export default App;
